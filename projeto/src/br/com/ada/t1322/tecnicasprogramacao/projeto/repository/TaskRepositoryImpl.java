af89e26dd207ab8ee2bc6222c1a098d27b6c9633
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-14 22:05:30 -0300

    Criação das classes: TaskControllerImpl, TaskServiceImpl e TaskRepositoryImpl

---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/repository/TaskRepositoryImpl.java
@@ -0,0 +1,4 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto.repository;
+
+public class TaskRepositoryImpl {
+}
---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/service/TaskServiceImpl.java
@@ -0,0 +1,4 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto.service;
+
+public class TaskServiceImpl {
+}
