f9a2ad8cbfd73686708bcec213466378b8fa0906
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-17 21:00:52 -0300

    TaskRepositoryImpl atualizado

--- projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/repository/TaskRepositoryImpl.java
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/repository/TaskRepositoryImpl.java
@@ -2,37 +2,55 @@ package br.com.ada.t1322.tecnicasprogramacao.projeto.repository;
 
 import br.com.ada.t1322.tecnicasprogramacao.projeto.model.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+
 
 public class TaskRepositoryImpl implements TaskRepository {
+    private List<Task> tasks = new ArrayList<>();
+    private long count = 1;
+
     @Override
     public Task save(Task task) {
-        return null;
+        if (!tasks.isEmpty()) {
+            count = tasks.size();
+            count++;
+            task.setId(count);
+            tasks.add(task);
+            return task;
+        } else {
+            task.setId(count);
+            tasks.add(task);
+            return task;
+        }
     }
 
     @Override
     public List<Task> findAll() {
-        return List.of();
+        return tasks;
     }
 
     @Override
     public List<Task> findByStatus(String status) {
-        return List.of();
+        //Predicate<Task> verificarStatus = task -> task.getStatus().getDescricao().equalsIgnoreCase(status);
+        Function<String, List<Task>> verificarStatus = s -> s.compareToIgnoreCase(status) == 0 ? tasks : List.of();
+        return (List<Task>) verificarStatus;
     }
 
     @Override
     public Optional<Task> findById(Long id) {
-        return Optional.empty();
+        return findById(id);
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        return tasks.removeIf(task -> task.getId().equals(id));
     }
 
     @Override
     public void deleteAll() {
-
+        tasks.clear();
     }
 }
